ce2d87890ab29c8db5ba0fcd99fe4f77ab895537
Paulo Alegria <paulo33alegria@gmail.com>
2021-12-10 12:11:54 +0000

    ficheiros X

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegularPolygon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
